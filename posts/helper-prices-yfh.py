81e0b1bf7ca70eab78a76135289fb965fb088b61
Jason Foster <jason.j.foster@gmail.com>
2025-09-24 07:53:06 -0400

    Version: 1.0.0

--- posts/helper-prices-yfh.py
+++ posts/helper-prices-yfh.py
@@ -19,34 +19,6 @@ def get_col(data_ls, col):
 
   return result_df
 
-# def get_data(tickers, scale, width, factors, returns_df = None, overlap_df = None):
-# 
-#   prices_ls = yfh.get_data(tickers)
-#   prices_df = get_col(prices_ls, "adjclose")
-#   tickers = prices_df.columns
-# 
-#   returns_df = pd.concat([returns_df, np.log(prices_df).diff()], axis = 1)
-#   overlap_df = pd.concat([overlap_df, returns_df[tickers].rolling(scale["overlap"], min_periods = 1).mean()], axis = 1)
-# 
-#   # weights = np.array([0.9 ** i for i in range(width - 1, -1, -1)]).reshape((width, 1))
-#   weights = np.array([1] * width).reshape((width, 1))
-# 
-#   overlap_df = overlap_df.dropna()
-#   overlap_x_df = overlap_df.dropna()[factors][-width:] # same dimension as `weights`
-#   overlap_y_df = overlap_df.dropna()[tickers][-width:]
-# 
-#   result = {
-#     "prices_df": prices_df,
-#     "returns_df": returns_df,
-#     "overlap_df": overlap_df,
-#     "overlap_x_df": overlap_x_df,
-#     "overlap_y_df": overlap_y_df,
-#     "weights": weights,
-#     "tickers": tickers,
-#   }
-# 
-#   return result
-
 prices_ls = yfh.get_data(tickers)
 prices_df = get_col(prices_ls, "adjclose")
 tickers = prices_df.columns
