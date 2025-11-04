ca35c81d57a7bee29351c04dcfd66f1621d36b49
Jason Foster <jason.j.foster@gmail.com>
2025-11-04 13:32:33 -0500

    Version: 1.0.0

--- posts/helper-levels.r
+++ posts/helper-levels.r
@@ -1,20 +1,64 @@
-library(quantmod)
-library(roll)
-library(data.table)
-options("getSymbols.warning4.0" = FALSE)
+# library(roll)
+# library(data.table)
+# options("getSymbols.warning4.0" = FALSE)
 
-factors <- c(factors_r, factors_d)
-width <- 252
-scale <- list("periods" = 252, "overlap" = 5)
+if (!exists("width", inherits = TRUE)) {
+  width <- 252
+}
 
-getSymbols(factors, src = "FRED")
-levels_xts <- do.call(merge, c(lapply(factors, function(i) get(i)), all = TRUE))
+if (!(exists("scale", inherits = TRUE) && is.list(get("scale", inherits = TRUE)))) {
+  scale <- list("periods" = 252, "overlap" = 5)
+}
 
-returns_xts <- do.call(merge, lapply(factors, function(i) {
-  if (i %in% factors_r) {
-    diff(log((levels_xts[ , i])))
-  } else if (i %in% factors_d) {
-    -diff(levels_xts[ , i]) / 100
-  }    
-}))
-overlap_xts <- roll_mean(returns_xts, scale[["overlap"]], min_obs = 1, na_restore = TRUE)
+status_r <- exists("factors_r", inherits = TRUE) && length(factors_r) > 0
+status_d <- exists("factors_d", inherits = TRUE) && length(factors_d) > 0
+
+if (status_r && status_d) {
+  factors <- unique(c(factors_r, factors_d))
+} else if (status_r) {
+  factors <- unique(factors_r)
+} else if (status_d) {
+  factors <- unique(factors_d)
+} else {
+  factors <- character(0)
+}
+
+if (length(factors) > 0) {
+  
+  quantmod::getSymbols(factors, src = "FRED")
+  
+  levels_xts <- do.call(merge, c(
+    lapply(factors, function(i) {
+      z <- get(i, inherits = TRUE)
+      colnames(z) <- i
+      z
+    }),
+    list(all = TRUE)))
+  
+  if (!exists("returns_xts", inherits = TRUE)) {
+    
+    returns_xts <- do.call(merge, c(
+      lapply(factors, function(i) {
+        if (status_r && (i %in% factors_r)) {
+          diff(log(levels_xts[, i]))
+        } else if (status_d && (i %in% factors_d)) {
+          -diff(levels_xts[, i]) / 100
+        } else {
+          NULL
+        }
+      }),
+      list(all = TRUE))
+    )
+    
+    if (!is.null(returns_xts) && (ncol(returns_xts) > 0)) {
+      colnames(returns_xts) <- factors
+    }
+    
+  }
+  
+  if (exists("returns_xts", inherits = TRUE) && !is.null(returns_xts) && (ncol(returns_xts) > 0)) {
+    overlap_xts <- roll::roll_mean(returns_xts, scale[["overlap"]],
+                                   min_obs = 1, na_restore = TRUE)
+  }
+  
+}
--- posts/helper-prices-yfh.R
+++ posts/helper-prices-yfh.R
@@ -1,48 +1,68 @@
+# library(quantmod)
+# library(yfhist)
+# library(roll)
+# library(data.table)
+# options("getSymbols.warning4.0" = FALSE)
+
+if (!exists("width", inherits = TRUE)) {
+  width <- 252
+}
+
 if (!(exists("scale", inherits = TRUE) && is.list(get("scale", inherits = TRUE)))) {
   scale <- list("periods" = 252, "overlap" = 5)
 }
 
-get_col <- function(data_ls, col) {
+if (!exists("weights", inherits = TRUE)) {
   
-  symbols <- names(data_ls)
-  
-  series_ls <- lapply(symbols, function(symbol) {
-    
-    df <- data_ls[[symbol]]
-    df <- df[ , c("index", col)]
-    
-    x <- xts::xts(df[[col]], order.by = df[["index"]])
-    colnames(x) <- symbol
-    
-    x
-    
-  })
-  
-  result_xts <- do.call(merge, c(series_ls, list(all = TRUE)))
-  
-  return(result_xts)
+  # weights <- 0.9 ^ ((width - 1):0)
+  weights <- rep(1, width)
   
 }
 
-data_ls <- yfhist::get_data(tickers)
-prices_xts <- get_col(data_ls, "adjclose")
-tickers <- colnames(prices_xts)
-
-if (exists("returns_xts", inherits = TRUE)) {
-  returns_xts <- merge(returns_xts, diff(log(prices_xts)))
-} else {
-  returns_xts <- diff(log(prices_xts))
+if (exists("tickers", inherits = TRUE) && (length(tickers) > 0)) {
+  
+  data_ls <- yfhist::get_data(tickers)
+  prices_xts <- xts::as.xts(yfhist::get_col(data_ls, "adjclose"))
+  tickers <- colnames(prices_xts)
+  
+  if (exists("factors", inherits = TRUE) && (length(factors) > 0)) {
+    if (exists("returns_xts", inherits = TRUE) && (ncol(returns_xts) > 0)) {
+      
+      returns_xts <- do.call(merge, c(list(returns_xts, diff(log(prices_xts))), list(all = TRUE)))
+      colnames(returns_xts) <- c(factors, tickers)
+      
+    }
+    
+    if (exists("overlap_xts", inherits = TRUE) && (ncol(overlap_xts) > 0)) {
+      
+      
+      overlap_xts <- do.call(merge, c(
+        list(overlap_xts,
+             roll::roll_mean(returns_xts[ , tickers], scale[["overlap"]],
+                             min_obs = 1, na_restore = TRUE)),
+        list(all = TRUE)))
+      colnames(overlap_xts) <- c(factors, tickers)
+      
+    } else {
+      overlap_xts <- roll::roll_mean(returns_xts[ , tickers], scale[["overlap"]],
+                                     min_obs = 1, na_restore = TRUE)
+    }
+    
+  } else {
+    
+    returns_xts <- diff(log(prices_xts))
+    overlap_xts <- roll::roll_mean(returns_xts[ , tickers], scale[["overlap"]],
+                                   min_obs = 1, na_restore = TRUE)
+    
+  }
+  
+  # if (exists("factors", inherits = TRUE) && (length(factors) > 0) &&
+  #     exists("overlap_xts", inherits = TRUE) && (ncol(overlap_xts) > 0)) {
+  #   
+  #   overlap_xts <- na.omit(overlap_xts)
+  #   overlap_x_xts <- tail(overlap_xts[ , factors], width) # same dimension as `weights`
+  #   overlap_y_xts <- tail(overlap_xts[ , tickers], width)
+  #   
+  # }
+  
 }
-
-if (exists("overlap_xts", inherits = TRUE)) {
-  overlap_xts <- merge(overlap_xts, roll::roll_mean(returns_xts[ , tickers], scale[["overlap"]], min_obs = 1))
-} else {
-  overlap_xts <- roll::roll_mean(returns_xts[ , tickers], scale[["overlap"]], min_obs = 1)
-}
-
-# weights <- 0.9 ^ ((width - 1):0)
-weights <- rep(1, width)
-
-overlap_xts <- na.omit(overlap_xts)
-overlap_x_xts <- tail(overlap_xts[ , factors], width) # same dimension as `weights`
-overlap_y_xts <- tail(overlap_xts[ , tickers], width)
